372de9c6be0ce10c5c3ca537766a9af4fe15648e
AnupisBoss <110639760+AnupisBoss@users.noreply.github.com>
2023-01-05 13:09:56 -0800

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,20 @@
+#Print Statement
+print("Your-Name")
+
+#Types
+'String' or "String"
+0-Integer
+4.221-FloatingPoint
+True or False - Boolean
+
+#Printing Other Types
+print(0)
+print(3.22)
+print(True)
+
+# Line By Line Interpretation
+print(True)
+print(False)
+
+#Parenthesis First
+print('%'*10)

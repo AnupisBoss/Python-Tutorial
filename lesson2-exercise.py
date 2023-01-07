ca81fe105016eb72120983ae2a6e54b201589332
AnupisBoss <110639760+AnupisBoss@users.noreply.github.com>
2023-01-07 14:33:44 -0800

    Add files via upload

---
+++ lesson2-exercise.py
@@ -0,0 +1,13 @@
+# Find the area of a regular hexagon where a is the length of one of the sides.
+# A should be able to be any value!
+# The formula to find this is:
+# ((3√3)➗2) ✖ (a2)
+# Square root is also to the power ½
+# Put all fractions in () otherwise it might not work!
+
+
+a = 6
+first_half = (3*(3**(1/2)))/2
+second_half = a**2
+area = ((first_half))*(a**2)
+print(area)
---
+++ lesson2.py
@@ -0,0 +1,16 @@
+#Operators
+print(5 + 4)
+print(5 - 4)
+print(5 * 4)
+print(5 / 1) -> FloatingPoint
+print(7 // 2) -> Integer
+print(2 ** 3)
+print(3 % 2)
+
+#Variables
+integer = 9
+print(integer)
+
+#Assignment Operators
+integer += 2
+print(integer)
